210c141a3b05ebc0c88d4ac36d84060feda9a9f3
thanhsang22107 <thanhsang22107@gmail.com>
2026-03-23 00:25:27 +0700

    Add files via upload

--- QuickSort_Ovr.cpp
+++ QuickSort_Ovr.cpp
@@ -12,12 +12,35 @@ int partition_Lomuto(int a[], int left,int right){
     swap(a[i+1],a[right]);
     return i+1;
 }
-void Quicksort(int a[], int left,int right){
+void Quicksort_Lomuto(int a[], int left,int right){
     if(left<right){
         int pi=partition_Lomuto(a,left,right);
-        Quicksort(a,left,pi-1);// ben trai pivot
-        Quicksort(a,pi+1,right);// ben phai pivot
+        Quicksort_Lomuto(a,left,pi-1);// ben trai pivot
+        Quicksort_Lomuto(a,pi+1,right);// ben phai pivot
     }
+}
+int partition_Hoare(int a[], int left, int right){
+    int i=left-1;
+    int j=right+1;
+    int pi=a[left];
+    while(true){
+        do{
+            i++;
+        }while(a[i]<pi);
+        do{
+            j--;
+        }
+    while(a[j]>pi);
+    if(i>=j) return j;
+    swap(a[i],a[j]);
+    }
+}
+void Quicksort_Hoare(int a[], int left, int right){
+    if(left<right){
+        int pi2=partition_Hoare(a,left,right);
+        Quicksort_Hoare(a,left,pi2);
+        Quicksort_Hoare(a,pi2+1,right);
+        }
 }
 int main(){
     int n;
@@ -26,7 +49,8 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
-    Quicksort(a,0,n-1);
+    // Quicksort_Lomuto(a,0,n-1);
+    Quicksort_Hoare(a,0,n-1);
     for(int i=0;i<n;i++)
     {
         cout<<a[i]<<" ";
